dd2874b20e560052c03a4b63d2149b72d98430c5
pavel <pavel210302@yandex.ru>
2021-12-23 15:26:27 +0500

    Убрал iframe, теперь js

---
+++ js/paste.js
@@ -0,0 +1,12 @@
+$('#img_text').load('..//levels/1hod/img_text.html');
+$('#main_text').load('..//levels/1hod/main_text.html');
+$('#aswers_list').load('..//levels/1hod/answers.html');
+function answer_1() {
+  console.log("1ready");
+};
+function answer_2() {
+  console.log("2ready");
+};
+function answer_3() {
+  console.log("3ready");
+};
