bd8e53a6733f199b6d1f1cfc91aa8f6a7fe55a37
pavel <pavel210302@yandex.ru>
2022-01-14 17:33:38 +0500

    Теперь читает txt файлы

--- js/paste.js
+++ js/paste.js
@@ -1,6 +1,6 @@
-$('#img_text').load('..//levels/1hod/img_text.html');
-$('#main_text').load('..//levels/1hod/main_text.html');
-$('#aswers_list').load('..//levels/1hod/answers.html');
+$('#img_text').load('..//levels/1hod/img_text.txt');
+$('#main_text').load('..//levels/1hod/main_text.txt');
+$('#aswers_list').load('..//levels/1hod/answers.txt');
 function answer_1() {
   console.log("1ready");
 };
