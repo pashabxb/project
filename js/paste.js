4c6721765e822b5de4d8e074fb20216137a50c51
pavel <pavel210302@yandex.ru>
2022-01-15 18:24:41 +0500

    Игра работает, нет конца игры и подсчёта очков

--- js/paste.js
+++ js/paste.js
@@ -1,12 +1,108 @@
-$('#img_text').load('..//levels/1hod/img_text.txt');
-$('#main_text').load('..//levels/1hod/main_text.txt');
-$('#aswers_list').load('..//levels/1hod/answers.txt');
+
+var dialog = document.querySelector('dialog');
+var level_number = 1;
+var showDialog = true;
+var answer = 1;
+var end_game = false;
+
+document.querySelector('#close').onclick = function() {
+  dialog.close();
+};
+
+
+  $('#start_newgame').hide();
+  $('#main_img').attr('src','..//img/hod/img'+ level_number +'.png')
+  $('#img_text').load('..//levels/'+ level_number +'hod/img_text.txt');
+  $('#main_text').load('..//levels/'+ level_number +'hod/main_text.txt');
+  $('#aswers_list').load('..//levels/'+ level_number +'hod/answers.txt');
+
+
+  document.querySelector('#show').onclick = function() {
+    if (showDialog == true){
+      dialog.showModal();
+    };
+    if (showDialog == false){
+      level_number=level_number+1;
+      if (level_number>14){
+        level_number = 14;
+      }
+
+    $('#level_number').text(level_number+'/14')
+    $('#main_img').attr('src','..//img/hod/img'+ level_number +'.png')
+    $('#img_text').load('..//levels/'+ level_number +'hod/img_text.txt');
+    $('#main_text').load('..//levels/'+ level_number +'hod/main_text.txt');
+    $('#aswers_list').load('..//levels/'+ level_number +'hod/answers.txt');
+    showDialog = true;
+    };
+};
+
+
+
 function answer_1() {
-  console.log("1ready");
+  answer = 1;
 };
 function answer_2() {
-  console.log("2ready");
+  answer = 2;
 };
 function answer_3() {
-  console.log("3ready");
+  answer = 3;
 };
+
+document.querySelector('#give_answer').onclick = function() {
+  dialog.close();
+  showDialog = false;
+  $('#main_text').load('..//levels/'+ level_number +'hod/end_text'+answer+'.txt');
+}
+
+/*  if (answer = 1){
+    if (level_number == 2 || level_number == 6 ||
+      level_number == 11 || level_number == 12 ){
+    end_game = true;
+  };
+  };
+  if (answer = 2){
+    if (level_number == 5 || level_number == 7 ||
+      level_number == 9 || level_number == 10 ||
+      level_number == 11 || level_number == 13){
+    end_game = true;
+  };
+  };
+
+  if (answer = 3) {
+    if (level_number == 1 || level_number == 3 ||
+      level_number == 4 || level_number == 8 ||
+      level_number == 9){
+    end_game = true;
+  };
+  };
+
+if(end_game=true){
+  dialog.close();
+  showDialog = true;
+  $('#main_text').load('..//levels/'+ level_number +'hod/end_text'+answer+'.txt');
+  $('#aswers_list').hide();
+  $('#start_newgame').show();
+  $('#give_answer').hide();
+};
+  if (end_game=false){
+      end_game= false;
+      dialog.close();
+      showDialog = false;
+      $('#main_text').load('..//levels/'+ level_number +'hod/end_text'+answer+'.txt');
+      answer= 1;
+};
+};
+
+/*document.querySelector('#start_newgame').onclick = function() {
+  end_game=false;
+  dialog.close();
+  showDialog = true;
+  a = 1;
+  $('#main_img').attr('src','..//img/hod/img'+ level_number +'.png')
+  $('#img_text').load('..//levels/'+ level_number +'hod/img_text.txt');
+  $('#main_text').load('..//levels/'+ level_number +'hod/main_text.txt');
+  $('#aswers_list').load('..//levels/'+ level_number +'hod/answers.txt');
+  $('#aswers_list').show();
+  $('#start_newgame').hide();
+  $('#give_answer').show();
+};*/
--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,8 @@
-var dialog = document.querySelector('dialog');
+/*var dialog = document.querySelector('dialog');
 document.querySelector('#show').onclick = function() {
   dialog.showModal();
 };
 document.querySelector('#close').onclick = function() {
   dialog.close();
 };
+*/
